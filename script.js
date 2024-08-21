3ab747288c5aa37078a737045138223fbecf5acc
Amanda Barylski <amandabarylski@live.com>
2024-08-21 16:25:48 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+/*Coding Steps:
+
+    Create a CRD application (CRUD without update) using json-server or another API
+    Use fetch and async/await to interact with the API
+    Use a form to create/post new entities
+    Build a way for users to delete entities
+    Include a way to get entities from the API and display them
+    You do NOT need update, but you can add it if you'd like
+    Use Bootstrap and/or CSS to style your project */
+
+/*After a brainstorming session, I decided I wanted to create a "commission board"
+where users could post, edit, and delete commissions. I am considering a toggle function
+to switch between "available" and "taken" states, but that will be the last thing I work
+on if I decide to do it at all.
+The commissions themselves will include a title, risk rating, and description.
+The title and description will be text inputs, and risk rating will be a dropdown menu.
+As I will be using json-server I will create a few to display when the page is opened.*/
