e92bf14d33f36943112d3738e18eea8e0608f97e
Amanda Barylski <amandabarylski@live.com>
2024-08-22 21:38:56 -0400

    worked on HTML and styling

--- script.js
+++ script.js
@@ -14,4 +14,9 @@ to switch between "available" and "taken" states, but that will be the last thin
 on if I decide to do it at all.
 The commissions themselves will include a title, risk rating, and description.
 The title and description will be text inputs, and risk rating will be a dropdown menu.
-As I will be using json-server I will create a few to display when the page is opened.*/
+As I will be using json-server I will create a few to display when the page is first opened.*/
+
+/*Coming back to this after working with the HTML and CSS, I have decided to adjust my plan.
+With how complicated I made the commissions, editing them would be difficult. I would likely
+have to place edit options for each section seperately. However, toggling them between two states
+would be more feasible, so I adjusted my buttons accordingly.*/
